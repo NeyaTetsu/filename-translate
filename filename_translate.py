ee96d637ba3c003448cd4e0e4f098e7cd03fd2ae
ねや <neya-tetsu@Neya-Macintosh.local>
2021-06-11 21:30:52 +0900

    GUIの配置を調整
    より見やすくなりました。

--- filename_translate.py
+++ filename_translate.py
@@ -92,27 +92,38 @@ def click1():
 #メインウィンドウ
 root = tk.Tk()
 root.title(u'FileName Translate')
-root.geometry('640x480')
+root.geometry('480x190')
+
+
+#フレーム1（モード選択）
+frame1 = tk.Frame(root)
+frame1.pack(fill='x')
 
 #ラベル1
-modetitle = tk.Label(text=u'翻訳モード')
-modetitle.pack()
+modetitle = tk.Label(frame1,text=u'翻訳モード',font=('','20'))
+modetitle.pack(side='left',anchor='w',fill='y',ipadx='50')
 #ラジオボタン（翻訳モード選択）
 mode = tk.IntVar()
 mode.set(0)
-mode1 = tk.Radiobutton(root,text=u'選択したファイル名を翻訳',value=0,variable=mode)
-mode1.pack()
-mode2 = tk.Radiobutton(root,text=u'選択したディレクトリ内のファイル名を翻訳',value=1,variable=mode)
-mode2.pack()
-mode3 = tk.Radiobutton(root,text=u'選択したディレクトリ名を翻訳',value=2,variable=mode)
-mode3.pack()
+mode1 = tk.Radiobutton(frame1,text=u'選択したファイル名を翻訳',value=0,variable=mode)
+mode1.pack(anchor='nw')
+mode2 = tk.Radiobutton(frame1,text=u'選択したディレクトリ内のファイル名を翻訳',value=1,variable=mode)
+mode2.pack(anchor='nw')
+mode3 = tk.Radiobutton(frame1,text=u'選択したディレクトリ名を翻訳',value=2,variable=mode)
+mode3.pack(anchor='nw')
+
+
+#フレーム2（パス指定）
+frame2 = tk.Frame(root)
+frame2.pack(fill='x')
 
 #ラベル2
-dialogtitle = tk.Label(text=u'パス')
-dialogtitle.pack()
+dialogtitle = tk.Label(frame2,text=u'パス',font=('','20'))
+dialogtitle.pack(side='left',anchor='w',ipadx='80')
 #ボタン（ファイル選択ダイアログを開く）
-dialog = tk.Button(root,text='参照',command=click1)
-dialog.pack()
+dialog = tk.Button(frame2,text='参照',command=click1)
+dialog.pack(anchor='w',fill='x')
+
 
 #チェックボックス1（ファイルをコピーするか否か）
 copy = tk.BooleanVar()
@@ -120,16 +131,21 @@ copy.set(True)
 copy0 = tk.Checkbutton(root,text=u'ファイルをサブディレクトリにコピーして、そのファイルを翻訳する。',variable=copy)
 copy0.pack()
 
+
+#フレーム3
+frame3 = tk.Frame(root)
+frame3.pack(fill='x')
+
 #ラベル3
-subtitle = tk.Label(text=u'サブディレクトリ名')
-subtitle.pack()
+subtitle = tk.Label(frame3,text=u'サブディレクトリ名',font=('','20'))
+subtitle.pack(side='left',anchor='w',ipadx='10')
 #テキストボックス2（サブディレクトリ名を指定）
-sub = tk.Entry(root)
+sub = tk.Entry(frame3)
 sub.insert(tk.END,'translated')
-sub.pack()
+sub.pack(anchor='w',fill='x')
 
 #ボタン4（処理）
-submit = tk.Button(root,text='処理開始',command=click2)
+submit = tk.Button(root,text='処理開始',command=click2,font=('','20'))
 submit.pack()
 
 root.mainloop()
